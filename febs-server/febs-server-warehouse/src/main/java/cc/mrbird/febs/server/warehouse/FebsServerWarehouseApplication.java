542befd8a16b89a377ab943205bd306b0d274702
1819268110 <yhk.123456>
2020-05-19 10:14:26 +0800

    仓库模块搭建

--- febs-server/febs-server-warehouse/src/main/java/cc/mrbird/febs/server/warehouse/FebsServerWarehouseApplication.java
+++ febs-server/febs-server-warehouse/src/main/java/cc/mrbird/febs/server/warehouse/FebsServerWarehouseApplication.java
@@ -1,9 +1,11 @@
 package cc.mrbird.febs.server.warehouse;
 
+import cc.mrbird.febs.common.security.starter.annotation.EnableFebsCloudResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableFebsCloudResourceServer
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class FebsServerWarehouseApplication {
 
